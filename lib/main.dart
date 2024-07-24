20162da80e94226675ed942f4b175e1ae3fc4eb1
bhavyarabhatt <bhavyarbhatt@gmail.com>
2024-07-25 00:34:24 +0530

    SharedPreferences Working

--- lib/main.dart
+++ lib/main.dart
@@ -23,21 +23,21 @@ class MyApp extends StatelessWidget {
 }
 
  
-class Helper {
-  static String valueSharedPreferences = '';
+// class Helper {
+//   static String valueSharedPreferences = '';
  
-// Write DATA
-static Future<bool> saveUserData(value) async {
-    SharedPreferences sharedPreferences = await SharedPreferences.getInstance();
-    return await sharedPreferences.setInt(valueSharedPreferences, value);
-  }
+// // Write DATA
+// static Future<bool> saveUserData(value) async {
+//     SharedPreferences sharedPreferences = await SharedPreferences.getInstance();
+//     return await sharedPreferences.setInt(valueSharedPreferences, value);
+//   }
    
-// Read Data
-static Future getUserData() async {
-    SharedPreferences sharedPreferences = await SharedPreferences.getInstance();
-    return sharedPreferences.getInt(valueSharedPreferences);
-  }
-}
+// // Read Data
+// static Future getUserData() async {
+//     SharedPreferences sharedPreferences = await SharedPreferences.getInstance();
+//     return sharedPreferences.getInt(valueSharedPreferences);
+//   }
+// }
 
 
 class MyHomePage extends StatefulWidget {
@@ -50,8 +50,52 @@ class MyHomePage extends StatefulWidget {
 class _MyHomePageState extends State<MyHomePage> {
  int _counter=0;
  
+@override
+  void initState() {
+    super.initState();
+    _loadCounter();
+  }
+
+void _loadCounter() async {
+    final prefs = await SharedPreferences.getInstance();
+    setState(() {
+      _counter = (prefs.getInt('counter') ?? 0 );
+    });
+}  
+
+
+
+//Incrementing counter after click
+void _incrementCounter()  async {
+  final prefs = await SharedPreferences.getInstance();
+  setState(() {
+    _counter = (prefs.getInt('counter') ?? 0) + 1;
+    	prefs.setInt('counter', _counter);
+  });
+}
+
   @override
   Widget build(BuildContext context) {
-    return  Placeholder();
+    return  Scaffold(
+      appBar: AppBar(
+        title: Text("Shared Preferences"),
+      ),
+      body: Center(
+        child: Column(
+          children: [
+            Text('You have pushed the button this many times'),
+            Text(
+              '$_counter',
+              ),
+            
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: _incrementCounter,
+        tooltip: "Increment",
+        child: Icon(Icons.add),
+      ),
+    );
   }
 }
